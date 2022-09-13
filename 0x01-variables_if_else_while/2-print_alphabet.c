73c7e092e91cbad07cb43bbdf008eb65427df4bb
IamXalim <Abdulxalim@yahoo.com>
2022-09-12 19:15:01 -0700

    task 2 done

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-
 /**
-*
 * main - print if the number is positive, zero, or negative
 *
 * Description: using the main function
@@ -11,10 +9,10 @@
 int main(void)
 {
 char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++);
+for (ch = 'a' ; ch <= 'z' ; ch++)
 {
 	putchar(ch);
 }
-putchar('\n');
+putchar('/n');
 return (0);
 }
